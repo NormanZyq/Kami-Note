6abfc16ccdea60f3750eb081c0abf7e878f1dbf6
TALKSWORD <15626555686@163.com>
2018-06-02 15:57:29 +0800

    新功能： ·情感分析 ·获得网络支持

--- app/src/main/java/fragment/NoteFragment.java
+++ app/src/main/java/fragment/NoteFragment.java
@@ -7,7 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.Utilities.Json.JSONObject;
 import com.example.zyq.kaminotetest.R;
+import com.example.zyq.kaminotetest.Utils.QcloudApiModuleCenter;
+import com.qcloud.Wenzhi;
+
+import java.util.TreeMap;
 
 /**
  * Created by TALK_SWORD on 2018/3/23.
@@ -36,5 +41,45 @@ public class NoteFragment extends Fragment {
         if(getArguments() != null){
             mfrom = getArguments().getString("from");
         }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                ApiCollector("黄沙百战穿金甲，不破楼兰终不还。");
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+
+    public void ApiCollector(String content){
+        TreeMap<String,Object> config = new TreeMap<String,Object>();
+        config.put("SecretId","AKIDe8NM5YehO4YroZDMN9fvynQq21mKkniR");
+        config.put("SecretKey","d4HH3h6E8m9H9l7C2dy2qOgrpa8TbVCf");
+        config.put("RequestMode","Post");
+        config.put("DefaultRegion","gz");
+
+        QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Wenzhi(),config);
+
+        TreeMap<String,Object> params = new TreeMap<String,Object>();
+        params.put("content",content);
+        params.put("type",2);
+
+        String result = null;
+        try{
+            result = module.call("TextSentiment",params);
+            JSONObject json_result = new JSONObject(result);
+            System.out.println(">>>>>>>>>>>>>>>go");
+            System.out.println(json_result);
+        }catch(Exception ex){
+            System.out.println(">>>>>>>>>>>>>>wrong");
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
