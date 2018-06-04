d40c3d747d8a499e79a7c50aa08ef98825836594
TALKSWORD <15626555686@163.com>
2018-06-04 22:35:18 +0800

    新功能： ·设置界面皮肤颜色（测试中未完成）

--- app/src/main/java/fragment/SettingsFragment.java
+++ app/src/main/java/fragment/SettingsFragment.java
@@ -1,29 +1,34 @@
 package fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.zyq.kaminotetest.Activity.Skin_Selector;
 import com.example.zyq.kaminotetest.R;
 
 /**
  * Created by TALK_SWORD on 2018/3/23.
  */
 
-public class CalendarFragment extends Fragment {
+public class SettingsFragment extends Fragment {
     private String mfrom;
+    private Button button;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate( R.layout.fragment_calendar,null);
+        View view = inflater.inflate( R.layout.fragment_settings,null);
+        button = view.findViewById(R.id.button);
         return view;
     }
 
-    public static CalendarFragment newInstance(String from){
-        CalendarFragment fragment = new CalendarFragment();
+    public static SettingsFragment newInstance(String from){
+        SettingsFragment fragment = new SettingsFragment();
         Bundle bundle = new Bundle();
         bundle.putString("from",from);
         fragment.setArguments(bundle);
@@ -35,5 +40,18 @@ public class CalendarFragment extends Fragment {
         if(getArguments() != null){
             mfrom = getArguments().getString("from");
         }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Skin_Selector.class);
+                startActivity(intent);
+            }
+        });
     }
 }
