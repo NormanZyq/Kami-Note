152812396cf388bccfebeaae44914d9b80b343d3
TALKSWORD <15626555686@163.com>
2018-06-07 22:22:27 +0800

    添加了一些注释

--- app/src/main/java/com/example/zyq/kaminotetest/Utils/MotionAnalyze.java
+++ app/src/main/java/com/example/zyq/kaminotetest/Utils/MotionAnalyze.java
@@ -1,5 +1,6 @@
 package com.example.zyq.kaminotetest.Utils;
 
+import com.example.zyq.kaminotetest.Class.MyNote;
 import com.example.zyq.kaminotetest.main.java.com.qcloud.Module.Wenzhi;
 import com.example.zyq.kaminotetest.main.java.com.qcloud.Utilities.Json.JSONObject;
 
@@ -7,7 +8,11 @@ import java.util.TreeMap;
 
 public class MotionAnalyze {
 
-    public static void getMotionStatistic(String content){
+    /**
+     * 调用api进行情感分析
+     * @param note 用户编辑的笔记//可用于更新note中的情感属性
+     */
+    public static void getMotionStatistic(MyNote note){
         TreeMap<String,Object> config = new TreeMap<>();
         config.put("SecretId","AKIDe8NM5YehO4YroZDMN9fvynQq21mKkniR");
         config.put("SecretKey","d4HH3h6E8m9H9l7C2dy2qOgrpa8TbVCf");
@@ -17,7 +22,7 @@ public class MotionAnalyze {
         QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Wenzhi(),config);
 
         TreeMap<String,Object> params = new TreeMap<>();
-        params.put("content",content);
+        params.put("content",note.getContent());
         params.put("type",2);
 
         String result = null;
@@ -25,8 +30,7 @@ public class MotionAnalyze {
             result = module.call("TextSentiment", params);
             JSONObject json_result = new JSONObject(result);
             Double positive = json_result.optDouble("positive");
-            System.out.println(json_result+">>>>>>>>>>>>>>>>>>>>>");
-            System.out.println(positive+">>>>>>>>>>>>>>>>>>>");
+            Double negative = json_result.optDouble("negative");
         }catch (Exception ex){
             System.out.println("error" + ex.getMessage());
         }
