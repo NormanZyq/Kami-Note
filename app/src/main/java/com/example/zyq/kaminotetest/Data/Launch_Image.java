a593713415d962b2eb220cc23e55b0c088f4d622
TALKSWORD <15626555686@163.com>
2018-06-10 14:13:00 +0800

    2018.6.10修复内容： 可以修改情感分析功能是否可用

---
+++ app/src/main/java/com/example/zyq/kaminotetest/Data/Launch_Image.java
@@ -0,0 +1,14 @@
+package com.example.zyq.kaminotetest.Data;
+
+import com.example.zyq.kaminotetest.R;
+
+public class Launch_Image {
+
+    public static final int[] Launch_PositiveImage_Chosen = {
+        R.drawable.ic_add_black
+    };
+
+    public static final int[] Launch_NegativeImage_Chosen = {
+            R.drawable.ic_add_black
+    };
+}
