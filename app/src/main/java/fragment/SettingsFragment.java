bb477710a2ad65e2e632dd7324d28927f4e96211
TALKSWORD <15626555686@163.com>
2018-06-10 14:03:26 +0800

    2018.6.10修复内容： 可以修改情感分析功能是否可用

--- app/src/main/java/fragment/SettingsFragment.java
+++ app/src/main/java/fragment/SettingsFragment.java
@@ -1,6 +1,8 @@
 package fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,8 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.zyq.kaminotetest.Activity.Skin_Selector;
+import com.example.zyq.kaminotetest.Data.DataClass;
 import com.example.zyq.kaminotetest.R;
 
 /**
@@ -19,11 +25,20 @@ import com.example.zyq.kaminotetest.R;
 public class SettingsFragment extends Fragment {
     private String mfrom;
     private Button button;
+    private Switch aSwitch;
+    private TextView textView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate( R.layout.fragment_settings,null);
         button = view.findViewById(R.id.button);
+        aSwitch = view.findViewById(R.id.switch1);
+        textView = view.findViewById(R.id.text);
+        if(DataClass.allowInternet){
+            aSwitch.setChecked(true);
+        }else{
+            aSwitch.setChecked(false);
+        }
         return view;
     }
 
@@ -51,6 +66,28 @@ public class SettingsFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(),Skin_Selector.class);
                 startActivity(intent);
+            }
+        });
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    textView.setText("开启");
+                    DataClass.allowInternet = true;
+                    SharedPreferences sharedPreferences = getActivity().getSharedPreferences("allowances", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("allowInternet",true);
+                    editor.apply();
+                }
+                else{
+                    textView.setText("关闭");
+                    DataClass.allowInternet = false;
+                    SharedPreferences sharedPreferences = getActivity().getSharedPreferences("allowances", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("allowInternet",false);
+                    editor.apply();
+                }
             }
         });
     }
